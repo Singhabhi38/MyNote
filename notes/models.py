98a192ed6f4ebc4fc387ecf563b5c39fdeffeffd
Singhabhi38 <abhi58singh@gmail.com>
2020-10-08 17:30:55 -0700

    MY project

---
+++ notes/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Signup(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    contact=models.CharField(max_length=30,null=True)
+    branch=models.CharField(max_length=10)
+    role=models.CharField(max_length=10,null=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Notes(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    uploadingdate=models.CharField(max_length=30)
+    branch=models.CharField(max_length=30)
+    subject=models.CharField(max_length=30)
+    notesfile=models.FileField(null=True)
+    uploadingdate=models.CharField(max_length=300)
+    filetype=models.CharField(max_length=300,null=True)
+    description=models.CharField(max_length=300,null=True)
+    status=models.CharField(max_length=15)
+
+
+    def __str__(self):
+        return self.signup.user.username+""+self.status
+
+
